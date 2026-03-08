febbe001083f96bbece85718123e08237d090bd3
刘元庆 <yqliu@jhinno.com>
2026-03-08 15:10:33 +0800

    feat: impl h-mamba

--- projects/mmdet3d_plugin/models/detectors/__init__.py
+++ projects/mmdet3d_plugin/models/detectors/__init__.py
@@ -1,6 +1,7 @@
 from .single_stage_fsd import SingleStageFSD, VoteSegmentor
 from .FSF import FSF
+from .FSF_HMamba import FSF_HMamba
 
 __all__ = [
-    'SingleStageFSD', 'VoteSegmentor','FSF'
+    'SingleStageFSD', 'VoteSegmentor', 'FSF', 'FSF_HMamba'
 ]
